463fa01115a833022de0ab85e327e3749fe24df3
pupil <pupil@ivkhk.ee>
2019-11-18 11:08:43 +0200

    files z disc

--- DEMO/Dizital/dizital/contact.php
+++ DEMO/Dizital/dizital/contact.php
@@ -152,31 +152,6 @@
 					</div>
 				</div>
 				
-				<!--Map Column-->
-				<div class="map-column col-lg-6 col-md-12 col-sm-12">
-					<div class="inner-column">
-						
-						<!--Map Info Section-->
-						<section class="map-section">
-
-							<!--Map Canvas-->
-							<div class="map-canvas"
-								data-zoom="12"
-								data-lat="-37.817085"
-								data-lng="144.955631"
-								data-type="roadmap"
-								data-hue="#ffc400"
-								data-title="Envato"
-								data-icon-path="images/icons/map-marker.png"
-								data-content="Melbourne VIC 3000, Australia<br><a href='mailto:info@youremail.com'>info@youremail.com</a>">
-							</div>
-
-						</section>
-						<!--End Map Info Section-->
-						
-					</div>
-				</div>
-				
 			</div>
 		</div>
 	</section>
